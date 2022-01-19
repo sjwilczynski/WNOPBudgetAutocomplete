bc0fffd0ea06efca4d83ef93d85b2cc87b5bb843
Stanisław <sjwilczynski@gmail.com>
2022-01-19 18:54:57 +0100

    use fluent ui controls

--- src/taskpane/components/AddTransactionForm.tsx
+++ src/taskpane/components/AddTransactionForm.tsx
@@ -1,12 +1,12 @@
 import * as React from "react";
-import { ComboBox, IComboBoxOption, SelectableOptionMenuItemType } from "@fluentui/react";
+import { ComboBox, DefaultButton, IComboBoxOption, SelectableOptionMenuItemType, TextField } from "@fluentui/react";
 import { Controller, useForm } from "react-hook-form";
 import { addTransaction } from "./addTransaction";
 
 const separator = "$%^";
 
 type Props = {
-  categories: { [key in string]: string[] } | undefined;
+  categories: { [key in string]: string[] };
 };
 
 type FormData = {
@@ -17,43 +17,46 @@ type FormData = {
 
 export const AddTransactionForm = ({ categories }: Props) => {
   const {
-    register,
     handleSubmit,
     control,
     formState: { errors },
   } = useForm<FormData>();
 
-  if (categories) {
-    const options = getOptions(categories);
-    return (
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="categoryDetails"
-          control={control}
-          render={({ field }) => (
-            <ComboBox
-              {...field}
-              options={options}
-              label="Wybierz kategorię"
-              autoComplete="on"
-              allowFreeform={true}
-              onChange={(_event, option) => {
-                field.onChange(option.data);
-              }}
-            />
-          )}
-        />
-        <p>{errors.categoryDetails?.message}</p>
-        <input type="number" {...register("day", { required: true, min: 1, max: 31, valueAsNumber: true })} />
-        <p>{errors.day?.message}</p>
-        <input type="number" {...register("price", { required: true, min: 0, valueAsNumber: true })} />
-        <p>{errors.price?.message}</p>
-        <button type="submit">Dodaj transakcję</button>
-      </form>
-    );
-  } else {
-    return null;
-  }
+  const options = getOptions(categories);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Controller
+        name="categoryDetails"
+        control={control}
+        render={({ field }) => (
+          <ComboBox
+            {...field}
+            options={options}
+            label="Wybierz kategorię"
+            autoComplete="on"
+            allowFreeform={true}
+            onChange={(_event, option) => {
+              field.onChange(option.data);
+            }}
+          />
+        )}
+      />
+      <p>{errors.categoryDetails?.message}</p>
+      <Controller
+        name="day"
+        control={control}
+        render={({ field }) => <TextField label="Dzień" type="number" {...field} value={field.value?.toString()} />}
+      />
+      <p>{errors.day?.message}</p>
+      <Controller
+        name="price"
+        control={control}
+        render={({ field }) => <TextField label="Cena" type="number" {...field} value={field.value?.toString()} />}
+      />
+      <p>{errors.price?.message}</p>
+      <DefaultButton type="submit">Dodaj transakcję</DefaultButton>
+    </form>
+  );
 };
 
 function onSubmit({ categoryDetails, day, price }: FormData): void {
