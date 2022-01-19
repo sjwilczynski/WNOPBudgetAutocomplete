3c2eede9931620302db05b33400fbc7030420625
Stanisław <sjwilczynski@gmail.com>
2022-01-19 19:29:48 +0100

    initial cleanup

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { AddTransactionForm } from "./AddTransactionForm";
-import Header from "./Header";
-import Progress from "./Progress";
+import { Header } from "./Header";
+import { Progress } from "./Progress";
 
 type Props = {
   title: string;
@@ -12,20 +12,12 @@ export default function App(props: Props) {
   const { title, categories } = props;
 
   if (!categories) {
-    return (
-      <Progress
-        title={title}
-        logo={require("./../../../assets/logo-filled.png")}
-        message="Please sideload your addin to see app body."
-      />
-    );
+    return <Progress title={title} message="Trwa ładowanie..." />;
   }
 
   return (
     <>
-      <div className="ms-welcome">
-        <Header logo={require("./../../../assets/logo-filled.png")} title={title} message="Welcome" />
-      </div>
+      <Header message="Witamy!" />
       <AddTransactionForm categories={categories}></AddTransactionForm>
     </>
   );
--- src/taskpane/components/Progress.tsx
+++ src/taskpane/components/Progress.tsx
@@ -1,22 +1,16 @@
 import * as React from "react";
 import { Spinner, SpinnerSize } from "@fluentui/react";
 
-export interface ProgressProps {
-  logo: string;
+type Props = {
   message: string;
   title: string;
-}
+};
 
-export default class Progress extends React.Component<ProgressProps> {
-  render() {
-    const { logo, message, title } = this.props;
-
-    return (
-      <section className="ms-welcome__progress ms-u-fadeIn500">
-        <img width="90" height="90" src={logo} alt={title} title={title} />
-        <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{title}</h1>
-        <Spinner size={SpinnerSize.large} label={message} />
-      </section>
-    );
-  }
+export function Progress({ message, title }: Props) {
+  return (
+    <section className="ms-welcome__progress ms-u-fadeIn500">
+      <h1 className="ms-fontSize-su ms-fontWeight-light ms-fontColor-neutralPrimary">{title}</h1>
+      <Spinner size={SpinnerSize.large} label={message} />
+    </section>
+  );
 }
