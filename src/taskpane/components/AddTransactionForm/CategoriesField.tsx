17fd3d50b54ad568a7d512137cc11bf3f7a9a0d2
Stanisław <sjwilczynski@gmail.com>
2023-01-14 18:16:29 +0100

    filter categories properly and add styling

--- src/taskpane/components/AddTransactionForm/CategoriesField.tsx
+++ src/taskpane/components/AddTransactionForm/CategoriesField.tsx
@@ -3,6 +3,7 @@ import { Control, Controller, FieldError } from "react-hook-form";
 import { ComboboxField, OptionGroup, Option } from "@fluentui/react-components/unstable";
 import { useTranslation } from "react-i18next";
 import { FormData, SEPARATOR } from "./formSchema";
+import "./CategoryField.css";
 
 type Props = {
   categories: Record<string, string[]>;
@@ -24,17 +25,19 @@ export const CategoriesField = ({ categories, control, error }: Props) => {
           {...field}
           label={t("choose-category")}
           autoComplete="on"
+          inlinePopup={true}
           onChange={(event) => {
             const value = event.target.value.trim();
             setCategoryFilter(value);
           }}
-          inlinePopup={true}
           onOptionSelect={(_event, data) => {
             field.onChange(data.optionValue);
           }}
           required={true}
           validationState={error ? "error" : "success"}
           validationMessage={error?.message}
+          listbox={{ className: "category-listbox" }}
+          onFocus={(e) => e.currentTarget.select()}
         >
           <CategoryOptions filter={categoryFilter} categories={categories} />
         </ComboboxField>
@@ -43,17 +46,29 @@ export const CategoriesField = ({ categories, control, error }: Props) => {
   );
 };
 
-const CategoryOptions = ({ filter, categories }: { filter: string; categories: Record<string, string[]> }) => (
-  <>
-    {Object.entries(categories).map(([category, subCategories]) => (
-      <OptionGroup key={category} label={category}>
-        {subCategories
-          .filter((subcategory) => subcategory.toLowerCase().indexOf(filter.toLowerCase()) > -1)
-          .map((subcategory) => {
-            const key = `${category}${SEPARATOR}${subcategory}`;
-            return <Option key={key} value={key} text={subcategory} />;
-          })}
-      </OptionGroup>
-    ))}
-  </>
+const CategoryOptions = React.memo(
+  ({ filter, categories }: { filter: string; categories: Record<string, string[]> }) => {
+    const matches = (filter: string, text: string) => text.toLowerCase().indexOf(filter.toLowerCase()) > -1;
+    const macthesFilter = (subcategory: string) => matches(filter, subcategory);
+    return (
+      <>
+        {Object.entries(categories)
+          .filter(([, subcatgeories]) => subcatgeories.some(macthesFilter))
+          .map(([category, subCategories]) => (
+            <OptionGroup key={category} label={category}>
+              {subCategories.filter(macthesFilter).map((subcategory) => {
+                const key = `${category}${SEPARATOR}${subcategory}`;
+                return (
+                  <Option key={key} value={key} text={subcategory}>
+                    {subcategory}
+                  </Option>
+                );
+              })}
+            </OptionGroup>
+          ))}
+      </>
+    );
+  }
 );
+
+CategoryOptions.displayName = "CategoryOptions";
