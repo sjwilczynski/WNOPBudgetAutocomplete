4a768d2f16bd9fad897e14dbbb9b5ecbda67b3f5
Stanisław <sjwilczynski@gmail.com>
2025-03-05 14:20:31 +0100

    add more tests

--- src/taskpane/components/App.stories.tsx
+++ src/taskpane/components/App.stories.tsx
@@ -1,13 +1,14 @@
 import { Decorator, Meta, StoryObj } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
+import { within, expect, userEvent, fn } from "@storybook/test";
 import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
 import App from "./App";
 import { ExcelProvider } from "../context/ExcelContext";
 import * as React from "react";
 
+const submitTransactionMock = fn();
 
 const excelDecorator: Decorator = (Story) => (
-  <ExcelProvider submitTransaction={action("submitTransaction")} month="January">
+  <ExcelProvider submitTransaction={submitTransactionMock} month="January">
     <Story />
   </ExcelProvider>
 );
@@ -36,3 +37,101 @@ export default meta;
 type Story = StoryObj<typeof App>;
 
 export const Default: Story = {};
+
+export const ValidForm: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const submitButton = await canvas.findByRole("button", { name: "Add transaction" });
+    expect(submitButton).toBeDisabled();
+    await fillInForm(canvas, "Car", "1", "100");
+    await expect(submitButton).toBeEnabled();
+    await userEvent.click(submitButton);
+    expect(submitTransactionMock).toHaveBeenCalledWith(
+      {
+        category: "Transportation",
+        subcategory: "Car",
+        day: 1,
+        price: 100,
+      },
+      expect.objectContaining({})
+    );
+  },
+};
+
+export const InvalidCategory: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const categoryField = await canvas.findByRole("combobox", { name: "Category" });
+    await userEvent.type(categoryField, "Invalid");
+    await userEvent.click(canvas.getByRole("spinbutton", { name: "Day" }));
+    const submitButton = canvas.getByRole("button", { name: "Add transaction" });
+    expect(submitButton).toBeDisabled();
+    expect(canvas.getByText("Subcategory is not valid")).toBeInTheDocument();
+    expect(categoryField).toBeInvalid();
+  },
+};
+
+export const InvalidDay: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const dayField = await canvas.findByRole("spinbutton", { name: "Day" });
+    await userEvent.type(dayField, "32");
+    const submitButton = canvas.getByRole("button", { name: "Add transaction" });
+    await userEvent.click(canvas.getByRole("spinbutton", { name: "Price" }));
+    expect(submitButton).toBeDisabled();
+    expect(canvas.getByText("day must be less than or equal to 31")).toBeInTheDocument();
+    expect(dayField).toBeInvalid();
+  },
+};
+
+export const InvalidPrice: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    const priceField = await canvas.findByRole("spinbutton", { name: "Price" });
+    await userEvent.type(priceField, "103124-");
+    await userEvent.click(canvas.getByRole("spinbutton", { name: "Day" }));
+    const submitButton = canvas.getByRole("button", { name: "Add transaction" });
+    expect(submitButton).toBeDisabled();
+    expect(canvas.getByText("Price must be a number")).toBeInTheDocument();
+    expect(priceField).toBeInvalid();
+  },
+};
+
+export const KeyboardNavigation: Story = {
+  play: async ({ canvasElement }) => {
+    const canvas = within(canvasElement);
+    await new Promise((resolve) => setTimeout(resolve, 500));
+    await fillInForm(canvas, "Car", "1", "100");
+    await userEvent.tab();
+    const submitButton = canvas.getByRole("button", { name: "Add transaction" });
+    const categoryField = canvas.getByRole("combobox", { name: "Category" });
+    const dayField = canvas.getByRole("spinbutton", { name: "Day" });
+    const priceField = canvas.getByRole("spinbutton", { name: "Price" });
+    expect(submitButton).toHaveFocus();
+    await userEvent.tab({ shift: true });
+    expect(priceField).toHaveFocus();
+    await userEvent.tab({ shift: true });
+    expect(dayField).toHaveFocus();
+    await userEvent.tab({ shift: true });
+    expect(categoryField).toHaveFocus();
+  },
+};
+
+const fillInForm = async (
+  canvas: ReturnType<typeof within>,
+  category: string,
+  day: string,
+  price: string
+) => {
+  const categoryField = await canvas.findByRole("combobox", { name: "Category" });
+  await userEvent.click(categoryField);
+  await userEvent.click(canvas.getByRole("option", { name: category }));
+  const dayField = await canvas.findByRole("spinbutton", { name: "Day" });
+  await userEvent.type(dayField, day);
+  const priceField = await canvas.findByRole("spinbutton", { name: "Price" });
+  await userEvent.type(priceField, price);
+};
