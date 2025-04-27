cefe5db4d3fb51f493d6bd78e27739e3d794a1de
Stanisław <sjwilczynski@gmail.com>
2025-04-27 14:47:56 +0200

    add lazy loading

--- src/taskpane/components/App.tsx
+++ src/taskpane/components/App.tsx
@@ -1,13 +1,21 @@
 import * as React from "react";
+import { lazy, Suspense } from "react";
 import { useTranslation } from "react-i18next";
-import { AddTransactionForm } from "./AddTransactionForm/AddTransactionForm";
 import { Header } from "./Header/Header";
 import { MonthLabel } from "./MonthLabel/MonthLabel";
 import { Progress } from "./Progress";
 import { TabList, Tab, makeStyles, tokens } from "@fluentui/react-components";
 import type { SelectTabData, SelectTabEvent } from "@fluentui/react-components";
-import { RatesView } from "./RatesView/RatesView";
 import { useExcel } from "../context/ExcelContext";
+
+const AddTransactionForm = lazy(() =>
+  import("./AddTransactionForm/AddTransactionForm").then((module) => ({
+    default: module.AddTransactionForm,
+  }))
+);
+const RatesView = lazy(() =>
+  import("./RatesView/RatesView").then((module) => ({ default: module.RatesView }))
+);
 
 type Props = {
   categories?: Record<string, string[]>;
@@ -28,7 +36,7 @@ const useStyles = makeStyles({
 
 export default function App({ categories }: Props) {
   const { t } = useTranslation();
-  const [selectedValue, setSelectedValue] = React.useState<string>("add-transaction");
+  const [selectedValue, setSelectedValue] = React.useState<string>("none");
   const styles = useStyles();
   const { month } = useExcel();
 
@@ -40,10 +48,13 @@ export default function App({ categories }: Props) {
   };
 
   React.useEffect(() => {
-    if (month === "NOT_FOUND" && selectedValue === "add-transaction") {
+    if (month === "NOT_FOUND" && selectedValue !== "rates") {
       setSelectedValue("rates");
     }
-  }, [month, selectedValue]);
+    if (isMonthView && selectedValue === "none") {
+      setSelectedValue("add-transaction");
+    }
+  }, [month, selectedValue, isMonthView]);
 
   return (
     <main className={styles.app}>
@@ -60,14 +71,17 @@ export default function App({ categories }: Props) {
             </Tab>
             <Tab value="rates">{t("tab-rates")}</Tab>
           </TabList>
-
-          {selectedValue === "add-transaction" && (
-            <>
-              <MonthLabel />
-              <AddTransactionForm categories={categories} />
-            </>
-          )}
-          {selectedValue === "rates" && <RatesView />}
+          <Suspense fallback={<Progress message={t("loading")} />}>
+            {selectedValue === "add-transaction" && (
+              <>
+                <MonthLabel />
+                <AddTransactionForm categories={categories} />
+              </>
+            )}
+          </Suspense>
+          <Suspense fallback={<Progress message={t("loading")} />}>
+            {selectedValue === "rates" && <RatesView />}
+          </Suspense>
         </>
       ) : (
         <Progress message={t("loading")} />
